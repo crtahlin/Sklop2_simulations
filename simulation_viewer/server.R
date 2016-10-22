27cd5d2389f258bef2153f27328ac534b398d78a
Crt Ahlin <crt.ahlin@gmail.com>
2016-10-22 14:04:28 +0200

    draft of viewer of the simulations

---
+++ simulation_viewer/server.R
@@ -0,0 +1,68 @@
+#
+# This is the server logic of a Shiny web application. You can run the 
+# application by clicking 'Run App' above.
+#
+# Find out more about building applications with Shiny here:
+# 
+#    http://shiny.rstudio.com/
+#
+
+library(shiny)
+library(DT)
+
+# Define server logic required to draw a histogram
+shinyServer(function(input, output) {
+   
+  output$select_folder <- renderUI({
+    # read folders holding results
+    folders <- list.dirs("../results")
+    # generate select input
+    selectInput("selected_folder",
+                "Select folder",
+                choices = folders,
+                selected = NULL)
+  })
+  
+  results_index <- reactive({
+    # read results in selected folder
+    load(file = paste0(input$selected_folder, "/results_index.Rdata"))
+    return(results_index)
+  })
+  
+  output$results_index <- DT::renderDataTable({
+    return(results_index())
+  })
+
+  # output$select_simluation <- renderUI({
+  #   selectInput("selected_simulation",
+  #               "Select simulation:",
+  #               choices = results_hashes(),
+  #               selected = NULL)
+  # })
+  # 
+  # results <- reactive({
+  #   result <- results_list()[[input$selected_simulation]]
+  #   return(result)
+  # })
+  # 
+  # output$plot_probability <- renderPlot({
+  #   curve(
+  #     sine_function(x, 
+  #                   par1sin = results()$par1sin,
+  #                   par2sin = results()$par2sin,
+  #                   par3sin = results()$par3sin,
+  #                   par4sin = results()$par4sin,
+  #                   par5sin = results()$par5sin,
+  #                   par1trend = results()$par1trend,
+  #                   par2trend = results()$par2trend,
+  #                   par3trend = results()$par3trend,
+  #                   par4trend = results()$par4trend,
+  #                   par5trend = results()$par5trend,
+  #                   add_trend = results()$add_trend,
+  #                   max_prob_value = results()$max_prob_value,
+  #                   min_prob_value = results()$min_prob_value),
+  #     from = 0, to = results()$tmax, main = "Probability function used in simulation")
+  #   
+  # })
+  
+})
---
+++ simulation_viewer/ui.R
@@ -0,0 +1,31 @@
+#
+# This is the user-interface definition of a Shiny web application. You can
+# run the application by clicking 'Run App' above.
+#
+# Find out more about building applications with Shiny here:
+# 
+#    http://shiny.rstudio.com/
+#
+
+library(shiny)
+library(peRiodic)
+
+# Define UI for application that draws a histogram
+shinyUI(fluidPage(
+  
+  # Application title
+  titlePanel("simulation results viewer"),
+  
+  sidebarPanel(
+    uiOutput("select_folder"),
+    uiOutput("select_simluation")
+  ),
+  
+  mainPanel(
+    DT::dataTableOutput("results_index")
+  #  textOutput("results_hashes"),
+  #  plotOutput("plot_probability")
+  )
+  
+  
+))
