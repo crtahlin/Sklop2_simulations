b7001cf215fd176ee328ee14e326f1618be23cd2
Crt Ahlin <crt.ahlin@gmail.com>
2016-10-24 22:10:28 +0200

    commit simulations code

--- code/test_different_cycle_lengths.R
+++ code/test_different_cycle_lengths.R
@@ -1,3 +1,7 @@
+detach("package:peRiodic", unload = TRUE)
+library(devtools)
+install_github("crtahlin/peRiodic", ref = "add_tmax")
+library(peRiodic)
 # test if different cycle lengths give same results
 
 n <- 100 # number of units
@@ -9,9 +13,6 @@ x365 <- x1 * 365 # x on [0, 365]
 
 # simulation of the probabilities
 x.transf.2 = x.transf = sine_function(x1) 
-
-# # but from now on, we have to transform the X to keep it inside [0,1] using modulus division, e.g. (x %% 1)
-# x <- x %% 1
 
 # simulation of the binary events
 y = ifelse(runif(n) < x.transf.2, 1, 0) # event happens if generated random number [0,1] smaller than simulated probability
@@ -52,7 +53,7 @@ mod.rcs.365 = glm(y~x.rcs.365, family="binomial")
 mod.cs.per.1 = glm(y~x.cs.per.1, family="binomial")
 mod.cs.per.365 = glm(y~x.cs.per.365, family="binomial")
 
-################### predicted probabilities, training ####
+################### predicted probabilities ####
 
 # estimated linear predictor with se
 # for rcs.per
@@ -74,19 +75,19 @@ pred.cs.per.train.365 = predict(mod.cs.per.365, se.fit=TRUE)
 pred.cs.per.train.1$fit - pred.cs.per.train.365$fit
 all.equal(pred.cs.per.train.1$fit, pred.cs.per.train.365$fit)
 
-##########################################333
-# saving the linear predictors
-lp.rcs.per.train = pred.rcs.per.train$fit
-lp.rcs.train = pred.rcs.train$fit
-lp.cs.per.train = pred.cs.per.train$fit
-
-# deriving the estimated probabilities
-p.rcs.per.train = 1/(1+exp(-lp.rcs.per.train))
-p.rcs.train = 1/(1+exp(-lp.rcs.train))
-p.cs.per.train = 1/(1+exp(-lp.cs.per.train))
-
-#estimated probabilities predictor with se
-p.pred.rcs.per.train = predict(mod.rcs.per, se.fit = TRUE, type="response")
-p.pred.rcs.train = predict(mod.rcs, se.fit=TRUE, type="response")
-p.pred.cs.per.train = predict(mod.cs.per, se.fit=TRUE, type="response")
-
+# ##########################################333
+# # saving the linear predictors
+# lp.rcs.per.train = pred.rcs.per.train$fit
+# lp.rcs.train = pred.rcs.train$fit
+# lp.cs.per.train = pred.cs.per.train$fit
+# 
+# # deriving the estimated probabilities
+# p.rcs.per.train = 1/(1+exp(-lp.rcs.per.train))
+# p.rcs.train = 1/(1+exp(-lp.rcs.train))
+# p.cs.per.train = 1/(1+exp(-lp.cs.per.train))
+# 
+# #estimated probabilities predictor with se
+# p.pred.rcs.per.train = predict(mod.rcs.per, se.fit = TRUE, type="response")
+# p.pred.rcs.train = predict(mod.rcs, se.fit=TRUE, type="response")
+# p.pred.cs.per.train = predict(mod.cs.per, se.fit=TRUE, type="response")
+# 
